57a45b665b0cbd8c1887e0df4bcfb86359a173b3
goodbedford <bedford@goodbedford.com>
2014-04-20 00:54:38 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CcsfLab::Application.config.secret_key_base = 'a86dbcb7a8772259fb15a5e7b9ddb94dac48c591dcc2f9439ac1d6d7148017a87861d3c1352a8ed0eaa5d38939e837a7d3528d17a64181d73e92517279ce90b5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CcsfLab::Application.config.session_store :cookie_store, key: '_ccsf_lab_session'
